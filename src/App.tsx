040bd0aa119b5142407fd08e6f3f2c34713b9941
vectorell <vlindell@outlook.com>
2023-10-31 14:50:28 +0100

    Made first version of recieved orders cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import './components/RecievedOrderCard.tsx'
+import RecievedOrderCard from './components/RecievedOrderCard.tsx'
 
 function App() {
 
 
   return (
     <>
-
+      <RecievedOrderCard />
     </>
   )
 }
