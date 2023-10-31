8946bd42a8295bbf8a69b00b9c40a3771993ad2e
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-10-31 14:48:45 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
+import Meals from "./components/Meals.jsx";
 
 function App() {
-
-
   return (
     <>
-
+      <Meals />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
