304503c1eb7f8280a1a2ead74132c1e20734af93
vectorell <vlindell@outlook.com>
2023-10-31 14:58:37 +0100

    completed merge

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import './App.css'
 import './components/RecievedOrderCard.tsx'
 import RecievedOrderCard from './components/RecievedOrderCard.tsx'
 import "./App.css";
-import Meals from "./components/Meals.jsx";
+import Meals from "./components/meals.jsx";
 
 function App() {
   return (
     <>
       <RecievedOrderCard />
-      <Meals />
     </>
   );
 }
