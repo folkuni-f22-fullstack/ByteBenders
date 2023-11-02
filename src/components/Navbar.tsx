8be7d747062a303b1046930c7fab76141013639a
vectorell <vlindell@outlook.com>
2023-11-02 15:11:09 +0100

    fixed merging problem

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,48 +3,36 @@ import { BsCart3, BsInfoLg, BsFillPersonFill } from "react-icons/bs";
 import "../styles/navbar.css";
 import { NavLink } from "react-router-dom";
 
-// interface MenuLinkProps {
-// 	linkto: string;
-// 	icon: React.ReactNode;
-// 	text: string;
-// }
+interface MenuLinkProps {
+    linkto: string;
+    icon: React.ReactNode;
+    text: string;
+}
 
-// const MenuLink: React.FC<MenuLinkProps> = ({ linkto, icon, text }) => {
-// 	return (
-// 		<div>
-// 			<a href={linkto}>
-// 				<div>{icon}</div>
-// 				<p>{text}</p>
-// 			</a>
-// 		</div>
-// 	);
-// };
+const MenuLink: React.FC<MenuLinkProps> = ({ linkto, icon, text }) => {
+    return (
+        <div>
+            <NavLink to={linkto}>
+                <div>{icon}</div>
+                <p>{text}</p>
+            </NavLink>
+        </div>
+    );
+};
 
-// const linkObjects = [
-// 	{ icon: <PiForkKnifeFill />, text: 'Menu' },
-// 	{ icon: <BsCart3 />, text: 'Cart' },
-// 	{ icon: <BsInfoLg />, text: 'About' },
-// 	{ icon: <BsFillPersonFill />, text: 'Log in' },
-// ];
+const linkObjects = [
+    { icon: <PiForkKnifeFill />, text: "Menu", to: "menu" },
+    { icon: <BsCart3 />, text: "Cart", to: "cart" },
+    { icon: <BsInfoLg />, text: "About", to: "information" },
+    { icon: <BsFillPersonFill />, text: "Log in", to: "login" },
+];
 
 const NavBar = () => {
     return (
         <nav className="navbar">
-            <NavLink to="menu">
-                <PiForkKnifeFill />
-            </NavLink>
-
-            <NavLink to="cart">
-                <BsCart3 />
-            </NavLink>
-
-            <NavLink to="information">
-                <BsInfoLg />
-            </NavLink>
-
-            <NavLink to="login">
-                <BsFillPersonFill />
-            </NavLink>
+            {linkObjects.map((link) => (
+                <MenuLink linkto={link.to} icon={link.icon} text={link.text} />
+            ))}
         </nav>
     );
 };
