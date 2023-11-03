bcdd1bc794ff707cd60fe5ee3bde6b64f33dbde7
Willi4mL <wi.lindblom@gmail.com>
2023-11-03 10:14:58 +0100

    get meals from detail route

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,39 +1,23 @@
-import role from '../assets/images/role.png'
-import salmon from '../assets/images/salmon.png'
+import { addToCart } from "../routes/ProductDetailsRoute.tsx";
 
 function CartCard() {
 
 	return (
 		<section>
-
 			<div className="cart-card-container">
-				<div className="cart-card">
-					<img className="cart-image" src={role} />
-					<p className='cart-name'>Californa roll 8st</p>
-					<p className='total-price'>218:-</p>
-					<p className='sub-text'>Lorem ipsum</p>
-					<div className='amount-container'>
-						<button className='sub'>-</button>
-						<p className='food-amount'>2</p>
-						<button className='plus'>+</button>
+				{addToCart.value.map(item => (
+					<div className="cart-card" key={item.name}>
+						<img className="cart-image" src={item.image} />
+						<p className='cart-name'> {item.name} </p>
+						<p className='sub-text'>Lorem ipsum</p>
+						<p className='total-price'> {item.price}:- </p>
+						<div className='amount-container'>
+							<button className='sub'>-</button>
+							<p className='food-amount'>2</p>
+							<button className='plus'>+</button>
+						</div>
 					</div>
-				</div>
-				<button className='customize-order'>Customize your order +</button>
-			</div>
-
-			<div className="cart-card-container">
-				<div className="cart-card">
-					<img className="cart-image" src={salmon} />
-					<p className='cart-name'>Salmon roll 8st</p>
-					<p className='total-price'>258:-</p>
-					<p className='sub-text'>Lorem ipsum</p>
-					<div className='amount-container'>
-						<button className='sub'>-</button>
-						<p className='food-amount'>2</p>
-						<button className='plus'>+</button>
-					</div>
-				</div>
-				<button className='customize-order'>Customize your order +</button>
+				))}
 			</div>
 		</section>
 	)
