07f8490fe4eb9ec9eb25fe2829112dbd7215909f
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-11-03 14:03:56 +0100

    added basic login form

--- src/routes/LoginRoute.tsx
+++ src/routes/LoginRoute.tsx
@@ -1,8 +1,21 @@
+import '../styles/login.css';
+
 export default function LoginRoute() {
-    return (
-        <div className="LoginRoute">
-            <h1> Login </h1>
-            <p>(text för test av routing, radera vid contentuppdatering)</p>
-        </div>
-    );
+	return (
+		<div className='LoginRoute'>
+			<form action='#' className='login-form'>
+				<div className='input-group'>
+					<label htmlFor='user-input'>Employee-number</label>
+					<input type='text' id='user-input' />
+				</div>
+
+				<div className='input-group'>
+					<label htmlFor='password-input'>Password</label>
+					<input type='text' id='password-input' />
+				</div>
+
+				<button className='login-button'>Log in</button>
+			</form>
+		</div>
+	);
 }
