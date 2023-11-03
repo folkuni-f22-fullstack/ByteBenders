d015d9fd131cb84ee27e62ccac9112201a0fad88
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-11-03 13:53:48 +0100

    categories added

--- src/components/Meals.tsx
+++ src/components/Meals.tsx
@@ -1,34 +1,70 @@
 import menuData from "../data/menu.json";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { BsCart3 } from "react-icons/bs";
 import "../styles/meals.css";
+import "../styles/categories.css";
 
 const Meals = () => {
+  const [selectedCategory, setSelectedCartegory] = useState("");
+
+  const filteredItems = menuData.filter((item) =>
+    selectedCategory ? item.category === selectedCategory : true
+  );
+
   return (
-    <section className="meals-section">
-      {menuData.map((menuItem) => (
-        <NavLink
-          key={menuItem.id}
-          to={`/menu/${menuItem.id}`}
-          className="meals-link"
+    <>
+      <section className="category-text-section">
+        <h4 className="category-header">Categories</h4>
+        <p className="category-text">
+          Select a category to explore our menu items
+        </p>
+      </section>
+      <section className="category-button-section">
+        <button
+          onClick={() => setSelectedCartegory("meals")}
+          className="category-button"
         >
-          <div className="meals-card">
-            <img
-              src={menuItem.image}
-              alt={`image of ${menuItem.name}`}
-              className="meals-img"
-            />
-            <div className="meals-text">
-              <p>{menuItem.name}</p>
-              <p className="meals-price">{menuItem.price} :-</p>
+          Meals
+        </button>
+        <button
+          onClick={() => setSelectedCartegory("sides")}
+          className="category-button"
+        >
+          Sides
+        </button>
+        <button
+          onClick={() => setSelectedCartegory("drinks")}
+          className="category-button"
+        >
+          Drinks
+        </button>
+      </section>
+      <section className="meals-section">
+        {filteredItems.map((menuItem) => (
+          <NavLink
+            key={menuItem.id}
+            to={`/menu/${menuItem.id}`}
+            className="meals-link"
+          >
+            <div className="meals-card">
+              <img
+                src={menuItem.image}
+                alt={`image of ${menuItem.name}`}
+                className="meals-img"
+              />
+              <div className="meals-text">
+                <p>{menuItem.name}</p>
+                <p className="meals-price">{menuItem.price} :-</p>
+              </div>
+              <button className="meals-btn">
+                Add to cart <BsCart3 className="btn-icon" />
+              </button>
             </div>
-            <button className="meals-btn">
-              Add to cart <BsCart3 className="btn-icon" />
-            </button>
-          </div>
-        </NavLink>
-      ))}
-    </section>
+          </NavLink>
+        ))}
+      </section>
+    </>
   );
 };
 export default Meals;
