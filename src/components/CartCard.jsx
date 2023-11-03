caa1fd8d13d469004d3de6f0034d000dafc4bbff
vectorell <vlindell@outlook.com>
2023-11-03 10:39:32 +0100

    Added routing to cart items

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,14 +1,28 @@
 import { addToCart } from "../routes/ProductDetailsRoute.tsx";
+import { NavLink } from 'react-router-dom';
+
+// <>
+// {menuData.map((menuItem, index) => (
+// 	<div key={index}>
+// 		<NavLink to={`/menu/${menuItem.id}`}>
+// 			<p> {menuItem.name} </p>
+// 		</NavLink>
+// 	</div>
+// ))}
+// </>
+// `/menu/${menuItem.id}`
 
 function CartCard() {
 
 	return (
 		<section>
 			<div className="cart-card-container">
-				{addToCart.value.map(item => (
-					<div className="cart-card" key={item.name}>
-						<img className="cart-image" src={item.image} />
-						<p className='cart-name'> {item.name} </p>
+				{addToCart.value.map((item, index) => (
+					<div className="cart-card" key={index}>
+						<NavLink to={`/menu/${item.id}`}>
+							<img className="cart-image" src={item.image} />
+						</NavLink>
+						<NavLink to={`/menu/${item.id}`} className='cart-name'> {item.name} </NavLink>
 						<p className='sub-text'>Lorem ipsum</p>
 						<p className='total-price'> {item.price}:- </p>
 						<div className='amount-container'>
