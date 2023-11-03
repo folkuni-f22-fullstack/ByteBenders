07b0b102047cea5077127b295592d9c5d8e70d92
vectorell <vlindell@outlook.com>
2023-11-03 15:33:19 +0100

    Added deal of the week, connected to local storage, checks if new week then new dishes applies.

--- src/routes/MenuRoute.tsx
+++ src/routes/MenuRoute.tsx
@@ -1,8 +1,10 @@
+import DealsOfTheWeek from "../components/DealsOfTheWeek.tsx";
 import Meals from "../components/Meals.jsx";
 
 export default function MenuRoute() {
     return (
         <div className="MenuRoute">
+            <DealsOfTheWeek />
             <Meals />
         </div>
     );
