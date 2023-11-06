abfcd8e9424f7a4616445c5ca14f2648c8f14d0b
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-11-06 15:49:13 +0100

    Implemented basic temporary login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,59 +1,73 @@
-import { PiForkKnifeFill } from "react-icons/pi";
-import { BsCart3, BsInfoLg, BsFillPersonFill } from "react-icons/bs";
-import { BiSolidPencil } from "react-icons/bi";
-import { GrUnorderedList } from "react-icons/gr";
-import { MdDone, MdLogout } from "react-icons/md";
-import "../styles/navbar.css";
-import { NavLink } from "react-router-dom";
+import { PiForkKnifeFill } from 'react-icons/pi';
+import { BsCart3, BsInfoLg, BsFillPersonFill } from 'react-icons/bs';
+import { BiSolidPencil } from 'react-icons/bi';
+import { GrUnorderedList } from 'react-icons/gr';
+import { MdDone, MdLogout } from 'react-icons/md';
+import '../styles/navbar.css';
+import { NavLink } from 'react-router-dom';
+import { loggedIn } from '../routes/LoginRoute.tsx';
+import { effect, signal } from '@preact/signals-react';
 
 // Skapar interface (=gränssnitt) som definierar vilka egenskaper och datatyper som förväntas.
 interface MenuLinkProps {
-  linkto: string;
-  icon: React.ReactNode;
-  text: string;
+	linkto: string;
+	icon: React.ReactNode;
+	text: string;
 }
 
 // Använder React.FC<MenuLinkProps> för att ange att MenuLink är en funktionell komponent som accepterar props enligt datatypen som anges av MenuLinkProps.
 const MenuLink: React.FC<MenuLinkProps> = ({ linkto, icon, text }) => {
-  return (
-    <div>
-      <NavLink to={linkto}>
-        <div>{icon}</div>
-        <p>{text}</p>
-      </NavLink>
-    </div>
-  );
+	return (
+		<div>
+			<NavLink to={linkto}>
+				<div>{icon}</div>
+				<p>{text}</p>
+			</NavLink>
+		</div>
+	);
 };
 
 // Ikoner för navbaren utan inloggning
 const linkObjects = [
-  { icon: <PiForkKnifeFill />, text: "Menu", to: "menu" },
-  { icon: <BsCart3 />, text: "Cart", to: "cart" },
-  { icon: <BsInfoLg />, text: "About", to: "information" },
-  { icon: <BsFillPersonFill />, text: "Log in", to: "login" },
+	{ icon: <PiForkKnifeFill />, text: 'Menu', to: 'menu' },
+	{ icon: <BsCart3 />, text: 'Cart', to: 'cart' },
+	{ icon: <BsInfoLg />, text: 'About', to: 'information' },
+	{ icon: <BsFillPersonFill />, text: 'Log in', to: 'login' },
 ];
 
 // Ikoner för navbaren efter inloggning
 const linkObjectsLoggedIn = [
-  { icon: <BiSolidPencil />, text: "Received", to: "" },
-  { icon: <GrUnorderedList />, text: "Current", to: "" },
-  { icon: <MdDone />, text: "Done", to: "" },
-  { icon: <MdLogout />, text: "Log out", to: "" },
+	{ icon: <BiSolidPencil />, text: 'Received', to: '' },
+	{ icon: <GrUnorderedList />, text: 'Current', to: '' },
+	{ icon: <MdDone />, text: 'Done', to: '' },
+	{ icon: <MdLogout />, text: 'Log out', to: '' },
 ];
 
+// variabel för att spara vilka ikoner som ska synas på navbaren
+const linksToShow = signal(linkObjects);
+
+// Uppdaterar vilka ikoner som ska synas beroende på vilket värde loggedIn-variabeln har
+effect(() => {
+	if (loggedIn.value === true) {
+		linksToShow.value = linkObjectsLoggedIn;
+	} else if (loggedIn.value === false) {
+		linksToShow.value = linkObjects;
+	}
+});
+
 const NavBar = () => {
-  return (
-    <nav className="navbar">
-      {linkObjects.map((link) => (
-        <MenuLink
-          key={link.text}
-          linkto={link.to}
-          icon={link.icon}
-          text={link.text}
-        />
-      ))}
-    </nav>
-  );
+	return (
+		<nav className='navbar'>
+			{linksToShow.value.map((link) => (
+				<MenuLink
+					key={link.text}
+					linkto={link.to}
+					icon={link.icon}
+					text={link.text}
+				/>
+			))}
+		</nav>
+	);
 };
 
 export default NavBar;
--- src/routes/LoginRoute.tsx
+++ src/routes/LoginRoute.tsx
@@ -1,9 +1,28 @@
 import { useState } from 'react';
+import { signal } from '@preact/signals-react';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import '../styles/login.css';
 
+export const loggedIn = signal(false);
+
 export default function LoginRoute() {
 	const [visible, setVisible] = useState(false);
+	const [userInput, setUserInput] = useState('');
+	const [passwordInput, setPasswordInput] = useState('');
+
+	const handleLogin = () => {
+		if (loggedIn.value === true) {
+			loggedIn.value = false;
+		} else {
+			if (userInput === '1234' && passwordInput === 'password') {
+				loggedIn.value = true;
+				setUserInput('');
+				setPasswordInput('');
+			} else {
+				console.log('Wrong user or password');
+			}
+		}
+	};
 
 	return (
 		<div className='LoginRoute'>
@@ -13,7 +32,12 @@ export default function LoginRoute() {
 				</div>
 				<div className='input-group'>
 					<label htmlFor='user-input'>Employee-number</label>
-					<input type='text' id='user-input' />
+					<input
+						type='text'
+						id='user-input'
+						value={userInput}
+						onChange={(event) => setUserInput(event.target.value)}
+					/>
 				</div>
 
 				<div className='input-group'>
@@ -22,6 +46,10 @@ export default function LoginRoute() {
 						<input
 							type={visible ? 'text' : 'password'}
 							id='password-input'
+							value={passwordInput}
+							onChange={(event) =>
+								setPasswordInput(event.target.value)
+							}
 						/>
 						<div
 							className='visibility-toggle'
@@ -32,7 +60,11 @@ export default function LoginRoute() {
 					</div>
 				</div>
 
-				<button type='submit' className='login-button'>
+				<button
+					type='submit'
+					className='login-button'
+					onClick={handleLogin}
+				>
 					Log in
 				</button>
 			</form>
