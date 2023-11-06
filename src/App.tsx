e803fd36b8512e95bba97cd314ed87326c713d3e
vectorell <vlindell@outlook.com>
2023-11-06 16:10:49 +0100

    initial commit, created component etc

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar.tsx";
 import "./styles/cartCard.css";
 import "./App.css";
+import CustomerStatusOrder from "./components/CustomerStatusOrder.tsx";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Outlet />
+      {/* <Outlet /> */}
+      <CustomerStatusOrder />
     </>
   );
 }
