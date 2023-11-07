ceed17ace475d42d18cef6d80e4e9a20162dd94f
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-11-07 14:17:04 +0100

    fixed error message

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -10,7 +10,7 @@ interface MenuLinkProps {
 // Använder React.FC<MenuLinkProps> för att ange att MenuLink är en funktionell komponent som accepterar props enligt datatypen som anges av MenuLinkProps.
 const MenuLink: React.FC<MenuLinkProps> = ({ linkto, icon, text }) => {
 	return (
-		<NavLink to={linkto} activeClassName='active'>
+		<NavLink to={linkto}>
 			<div className='nav-icon'>{icon}</div>
 			<p className='nav-text'>{text}</p>
 		</NavLink>
