6e92a84af78750c78a16953d7e7430f50a87c9f5
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-11-10 11:46:48 +0100

    details layout

--- src/routes/ProductDetailsRoute.tsx
+++ src/routes/ProductDetailsRoute.tsx
@@ -2,11 +2,11 @@ import { useParams } from "react-router-dom";
 import menuData from "../data/menu.json";
 import { useState, useEffect } from "react";
 import { BiArrowBack, BiMinus, BiPlus } from "react-icons/bi";
-import { BsCart3 } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
 import addToLS from "../utils/addCartLS";
 import { quantity } from "../utils/addCartLS.tsx";
 import "../styles/details.css";
+import CartRoute from "./CartRoute.tsx";
 
 // details code imported and implemented with original
 
@@ -27,63 +27,65 @@ export default function ProductDetailsRoute() {
   function updateQuantity(change) {
     // Cannot be less than one
     if (quantity.value >= 1) {
-      let newQuantity = quantity.value += change
-      newQuantity = Math.max(newQuantity, 1)
-      quantity.value = newQuantity
+      let newQuantity = (quantity.value += change);
+      newQuantity = Math.max(newQuantity, 1);
+      quantity.value = newQuantity;
     }
   }
 
   // Send to local storage
   function handleAddToCart() {
-    addToLS(id)
+    addToLS(id);
   }
 
   return (
     <>
       {product && (
         <main className="details-page">
-          <div className="back-container">
+          <section className="details-container">
             <NavLink to="/menu">
               <BiArrowBack className="return-arrow-icon" />
             </NavLink>
-          </div>
-          <div className="content-container">
             <img
               src={product.image}
               alt={`image of ${product.name}`}
-              className="background-img"
+              className="dish-img"
             />
-            <section className="detail-popup">
-              <section className="detail-content">
-                <section className="top-row">
-                  <div className="amount">
-                    <button
-                      className="amount-detail-button"
-                      onClick={() => updateQuantity(-1)}
-                    >
-                      <BiMinus className="sub-amount-icon" />
-                    </button>
-                    <div className="amount-count">{quantity}</div>
-                    <button
-                      className="amount-detail-button"
-                      onClick={() => updateQuantity(1)}
-                    >
-                      <BiPlus className="add-amount-icon" />
-                    </button>
-                  </div>
-                  <p className="price">{product.price * quantity} :-</p>
-                </section>
-              </section>
-              <div className="description-section">
-                <h5 className="detail-header">{product.name}</h5>
-                <p className="description-text">{product.description}</p>
+            <div className="amount-price">
+              <div className="detail-amount">
+                <button
+                  className="amount-detail-button"
+                  onClick={() => updateQuantity(-1)}
+                >
+                  <BiMinus className="sub-amount-icon" />
+                </button>
+                <div className="amount-count">{quantity}</div>
+                <button
+                  className="amount-detail-button"
+                  onClick={() => updateQuantity(1)}
+                >
+                  <BiPlus className="add-amount-icon" />
+                </button>
               </div>
-              <button className="cart-btn" onClick={handleAddToCart}>
-                <p className="btn-text">Add to Cart</p>
-                <BsCart3 className="add-to-cart-from-details-icon" />
-              </button>
-            </section>
-          </div>
+              <p className="detail-price">{product.price * quantity} :-</p>
+            </div>
+            <div className="details-text">
+              <h4 className="detail-header">{product.name}</h4>
+              {product.allergenes.length !== 0 &&
+                product.allergenes[0] !== "" && (
+                  <p className="allergenes-p">
+                    <span className="allergenes-span">Allergenes:</span>{" "}
+                    {product.allergenes}
+                  </p>
+                )}
+
+              <p className="description-text">{product.description}</p>
+            </div>
+            <button className="cart-btn" onClick={handleAddToCart}>
+              Add to Cart
+            </button>
+          </section>
+          <CartRoute />
         </main>
       )}
     </>
