db2d97177555119b24bff2ddebaed7493254f9e8
vectorell <vlindell@outlook.com>
2023-11-10 11:01:18 +0100

    commit before changing branch

--- src/constants/nav-links.tsx
+++ src/constants/nav-links.tsx
@@ -3,11 +3,19 @@ import { BsCart3, BsInfoLg, BsFillPersonFill } from 'react-icons/bs';
 import { BiSolidPencil } from 'react-icons/bi';
 import { GrUnorderedList } from 'react-icons/gr';
 import { MdDone, MdLogout } from 'react-icons/md';
+import { useRecoilState } from 'recoil';
+import { cartState } from '../recoil/cartNumberState.js'
+
+export default function fetchRecoil() {
+	const [cartItems, setCartItems] = useRecoilState(cartState)
+
+	return cartItems
+}
 
 // Ikoner för navbaren utan inloggning
 const linkObjects = [
 	{ icon: <PiForkKnifeFill />, text: 'Menu', to: 'menu' },
-	{ icon: <BsCart3 />, text: 'Cart', to: 'cart' },
+	{ icon: <BsCart3 />, text: `Cart (${fetchRecoil()})`, to: 'cart' },
 	{ icon: <BsInfoLg />, text: 'About', to: 'information' },
 	{ icon: <BsFillPersonFill />, text: 'Log in', to: 'login' },
 ];
