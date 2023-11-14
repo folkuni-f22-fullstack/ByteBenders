137ea92a92b8fd709a9abb964c94765c096d1c9c
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-11-14 10:14:36 +0100

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar.tsx";
+import { Header } from "./components/Header.tsx";
 import "./styles/cartCard.css";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Header />
       <NavBar />
       <Outlet />
     </>
