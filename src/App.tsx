57a05013c9866bd205613cd7b14961f3cfb6e3b7
Willi4mL <wi.lindblom@gmail.com>
2023-11-15 11:30:03 +0100

    removed header from aboute

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "./components/Navbar.tsx";
 import { Header } from "./components/Header.tsx";
 import "./styles/cartCard.css";
 import "./App.css";
 
 function App() {
+
+  const location = useLocation()
+  const showHeader = !'/information'.includes(location.pathname)
   return (
     <>
-      <Header />
+      {showHeader && <Header />}
       <NavBar />
       <Outlet />
     </>
