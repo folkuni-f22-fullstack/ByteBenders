ba0815b1ed978de1e4abbb9a850754f6d2182146
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-11-16 15:38:28 +0100

    counter styling

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -1,20 +1,29 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import { cartState } from "../recoil/cartNumberState.js";
+import { useRecoilState } from "recoil";
 
 // Skapar interface (=gränssnitt) som definierar vilka egenskaper och datatyper som förväntas.
 interface MenuLinkProps {
-	linkto: string;
-	icon: React.ReactNode;
-	text: string;
+  linkto: string;
+  icon: React.ReactNode;
+  text: string;
 }
 
 // Använder React.FC<MenuLinkProps> för att ange att MenuLink är en funktionell komponent som accepterar props enligt datatypen som anges av MenuLinkProps.
 const MenuLink: React.FC<MenuLinkProps> = ({ linkto, icon, text }) => {
-	return (
-		<NavLink to={linkto}>
-			<div className='nav-icon'>{icon}</div>
-			<p className='nav-text'>{text}</p>
-		</NavLink>
-	);
+  const [cartItems, setCartItems] = useRecoilState(cartState);
+
+  return (
+    <NavLink to={linkto}>
+      <div className="nav-icon">
+        {icon}
+        <p className="nav-text">{text}</p>
+        {text == "Cart" && cartItems > 0 && (
+          <div className="cart-item-count">{cartItems}</div>
+        )}
+      </div>
+    </NavLink>
+  );
 };
 
 export default MenuLink;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,120 +1,111 @@
-import { useRef, useState, useEffect } from 'react';
-import '../styles/navbar.css';
-import MenuLink from './MenuLink.tsx';
-import { useNavigate } from 'react-router-dom'
-import { PiForkKnifeFill } from 'react-icons/pi';
-import { BsCart3, BsInfoLg, BsFillPersonFill } from 'react-icons/bs';
-import { BiSolidPencil } from 'react-icons/bi';
-import { GrUnorderedList } from 'react-icons/gr';
-import { MdDone, MdLogout } from 'react-icons/md';
-import { useRecoilState } from 'recoil';
-import { loginState } from '../recoil/loginState.js'
-import { cartState } from '../recoil/cartNumberState.js'
-import { getCartQuantity } from '../utils/general.ts'
+import { useRef, useState, useEffect } from "react";
+import "../styles/navbar.css";
+import MenuLink from "./MenuLink.tsx";
+import { useNavigate } from "react-router-dom";
+import { PiForkKnifeFill } from "react-icons/pi";
+import { BsCart3, BsInfoLg, BsFillPersonFill } from "react-icons/bs";
+import { BiSolidPencil } from "react-icons/bi";
+import { GrUnorderedList } from "react-icons/gr";
+import { MdDone, MdLogout } from "react-icons/md";
+import { useRecoilState } from "recoil";
+import { loginState } from "../recoil/loginState.js";
+import { cartState } from "../recoil/cartNumberState.js";
+import { getCartQuantity } from "../utils/general.ts";
 
 const NavBar = () => {
-	
-	const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState)
-	const [cartItems, setCartItems] = useRecoilState(cartState)
-	const [linksToShow, setLinksToShow] = useState([])
-	const navigate = useNavigate()
-	const modal = useRef<HTMLDialogElement | null>(null);
-	
-	// Lägger in antal i kundvagn vid komponent-mount
-	useEffect(() => {
-		setCartItems(getCartQuantity())
-	})
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState);
+  const [cartItems, setCartItems] = useRecoilState(cartState);
+  const [linksToShow, setLinksToShow] = useState([]);
+  const navigate = useNavigate();
+  const modal = useRef<HTMLDialogElement | null>(null);
 
-	// Ikoner för navbaren utan inloggning
-	const linkObjects = [
-		{ icon: <PiForkKnifeFill />, text: 'Menu', to: 'menu' },
-		{ icon: <BsCart3 />, text: `Cart (${cartItems})`, to: 'cart' },
-		{ icon: <BsInfoLg />, text: 'About', to: 'information' },
-		{ icon: <BsFillPersonFill />, text: 'Log in', to: 'login' },
-	]
-	
-	const updatedLinkObjects = linkObjects.map(link => {
-		if (link.text === 'Cart') {
-		  return { ...link, text: `Cart (${cartItems})` };
-		}
-		return link;
-	  });
-	
-	// Ikoner för navbaren efter inloggning
-	const linkObjectsLoggedIn = [
-		{ icon: <BiSolidPencil />, text: 'Received', to: 'recieved' },
-		{ icon: <GrUnorderedList />, text: 'Current', to: 'current' },
-		{ icon: <MdDone />, text: 'Done', to: 'done' },
-		{ icon: <MdLogout />, text: 'Log out', to: '#' },
-	];
+  // Lägger in antal i kundvagn vid komponent-mount
+  useEffect(() => {
+    setCartItems(getCartQuantity());
+  });
 
-	useEffect(() => {
-		setLinksToShow(isLoggedIn ? linkObjectsLoggedIn : updatedLinkObjects)
-	  }, [isLoggedIn, cartItems]);
+  // Ikoner för navbaren utan inloggning
+  const linkObjects = [
+    { icon: <PiForkKnifeFill />, text: "Menu", to: "menu" },
+    { icon: <BsCart3 />, text: `Cart`, to: "cart" },
+    { icon: <BsInfoLg />, text: "About", to: "information" },
+    { icon: <BsFillPersonFill />, text: "Log in", to: "login" },
+  ];
 
+  const updatedLinkObjects = linkObjects.map((link) => {
+    if (link.text === "Cart") {
+      return { ...link, text: `Cart` };
+    }
+    return link;
+  });
 
-	const handleLogout = () => {
-		closeModal();
-		setIsLoggedIn(false);
-		navigate("")
-	};
+  // Ikoner för navbaren efter inloggning
+  const linkObjectsLoggedIn = [
+    { icon: <BiSolidPencil />, text: "Received", to: "recieved" },
+    { icon: <GrUnorderedList />, text: "Current", to: "current" },
+    { icon: <MdDone />, text: "Done", to: "done" },
+    { icon: <MdLogout />, text: "Log out", to: "#" },
+  ];
 
-	const confirmLogout = () => {
-		modal.current?.showModal();
-	};
+  useEffect(() => {
+    setLinksToShow(isLoggedIn ? linkObjectsLoggedIn : updatedLinkObjects);
+  }, [isLoggedIn, cartItems]);
 
-	const closeModal = () => {
-		modal.current?.close();
-	};
+  const handleLogout = () => {
+    closeModal();
+    setIsLoggedIn(false);
+    navigate("");
+  };
 
-	const outsideClick = (e: React.MouseEvent) => {
-		const dialogDimensions = modal.current?.getBoundingClientRect();
-		if (dialogDimensions) {
-			if (
-				e.clientX < dialogDimensions.left ||
-				e.clientX > dialogDimensions.right ||
-				e.clientY < dialogDimensions.top ||
-				e.clientY > dialogDimensions.bottom
-			) {
-				closeModal();
-			}
-		}
-	};
+  const confirmLogout = () => {
+    modal.current?.showModal();
+  };
 
-	return (
-		<nav className='navbar'>
-			{linksToShow && linksToShow.map((link) => (
-				<div key={link.text} className='link-container'>
-					{link.text === 'Log out' ? (
-						<button onClick={confirmLogout}>
-							<MenuLink
-								linkto={link.to}
-								icon={link.icon}
-								text={link.text}
-							/>
-						</button>
-					) : (
-						<MenuLink
-							linkto={link.to}
-							icon={link.icon}
-							text={link.text}
-						/>
-					)}
-				</div>
-			))}
+  const closeModal = () => {
+    modal.current?.close();
+  };
 
-			<dialog
-				className='modal'
-				ref={modal}
-				onClick={(e) => {
-					outsideClick(e);
-				}}
-			>
-				<p>Log out?</p>
-				<button onClick={handleLogout}>Confirm</button>
-			</dialog>
-		</nav>
-	);
+  const outsideClick = (e: React.MouseEvent) => {
+    const dialogDimensions = modal.current?.getBoundingClientRect();
+    if (dialogDimensions) {
+      if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+      ) {
+        closeModal();
+      }
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      {linksToShow &&
+        linksToShow.map((link) => (
+          <div key={link.text} className="link-container">
+            {link.text === "Log out" ? (
+              <button onClick={confirmLogout}>
+                <MenuLink linkto={link.to} icon={link.icon} text={link.text} />
+              </button>
+            ) : (
+              <MenuLink linkto={link.to} icon={link.icon} text={link.text} />
+            )}
+          </div>
+        ))}
+
+      <dialog
+        className="modal"
+        ref={modal}
+        onClick={(e) => {
+          outsideClick(e);
+        }}
+      >
+        <p>Log out?</p>
+        <button onClick={handleLogout}>Confirm</button>
+      </dialog>
+    </nav>
+  );
 };
 
 export default NavBar;
