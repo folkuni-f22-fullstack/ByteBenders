fd0c518cd1e116542d3afdc2a6cc07d00d26fefb
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-11-27 09:52:22 +0100

    refactored category-buttons

--- src/components/Meals.tsx
+++ src/components/Meals.tsx
@@ -1,140 +1,120 @@
-import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-import { BsCart3 } from "react-icons/bs";
-import "../styles/meals.css";
-import "../styles/categories.css";
-import CartRoute from "../routes/CartRoute";
-import addToLS from "../utils/addCartLS";
-import { Dish } from "../interfaces/dish.ts";
-import SearchBar from "./SearchBar.tsx";
-import { filterByCategory } from "../utils/filter.ts";
-import { useRecoilState } from "recoil";
-import { isCartEmptyState } from "../recoil/cartNumberState.js";
-import WindowSizeListener from "../utils/WindowListener.tsx";
-import { menuState } from "../recoil/menuState.js";
+import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { BsCart3 } from 'react-icons/bs';
+import '../styles/meals.css';
+import '../styles/categories.css';
+import CartRoute from '../routes/CartRoute';
+import addToLS from '../utils/addCartLS';
+import { Dish } from '../interfaces/dish.ts';
+import SearchBar from './SearchBar.tsx';
+import { filterByCategory } from '../utils/filter.ts';
+import { useRecoilState } from 'recoil';
+import { isCartEmptyState } from '../recoil/cartNumberState.js';
+import WindowSizeListener from '../utils/WindowListener.tsx';
+import { menuState } from '../recoil/menuState.js';
+import CategoryButton from './CategoryButton.tsx';
 // import { signal } from "@preact/signals-react";
 
 const Meals = () => {
-  const [selectedCategory, setSelectedCategory] = useState("all");
-  const [listToShow, setListToShow] = useState<Dish[]>([]);
-  const cartData = JSON.parse(localStorage.getItem("cart")) || [];
-  const [cartCopy, setCartCopy] = useState([...cartData]);
-  const [isCartEmpty, setIsCartEmpty] = useRecoilState(isCartEmptyState);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [fullMenu, setFullMenu] = useRecoilState<Dish[]>(menuState);
+	const [selectedCategory, setSelectedCategory] = useState('all');
+	const [listToShow, setListToShow] = useState<Dish[]>([]);
+	const cartData = JSON.parse(localStorage.getItem('cart')) || [];
+	const [cartCopy, setCartCopy] = useState([...cartData]);
+	const [isCartEmpty, setIsCartEmpty] = useRecoilState(isCartEmptyState);
+	const [errorMessage, setErrorMessage] = useState('');
+	const [fullMenu, setFullMenu] = useRecoilState<Dish[]>(menuState);
 
-  useEffect(() => {
-    setListToShow(filteredItems);
-  }, [selectedCategory, fullMenu]);
+	useEffect(() => {
+		setListToShow(filteredItems);
+	}, [selectedCategory, fullMenu]);
 
-  // Ursprungslistan som skickas med till sök och filter-funktionerna
-  const filteredItems: Dish[] =
-    selectedCategory === "all"
-      ? fullMenu
-      : filterByCategory(selectedCategory, fullMenu);
+	// Ursprungslistan som skickas med till sök och filter-funktionerna
+	const filteredItems: Dish[] =
+		selectedCategory === 'all'
+			? fullMenu
+			: filterByCategory(selectedCategory, fullMenu);
 
-  const windowWidth = WindowSizeListener();
+	const windowWidth = WindowSizeListener();
 
-  const handleCategoryClick = (category: string) => {
-    setSelectedCategory(category);
-  };
+	const handleCategoryClick = (category: string) => {
+		setSelectedCategory(category);
+	};
 
-  // Set value to 1
-  function refreshQuantity() {
-    quantity.value = 1;
-  }
+	// Set value to 1
+	function refreshQuantity() {
+		quantity.value = 1;
+	}
 
-  // Add to local storage
-  async function handleAddToCart(id: number) {
-    await addToLS(id, "/api/meals");
-    setIsCartEmpty(!isCartEmpty);
-  }
+	// Add to local storage
+	async function handleAddToCart(id: number) {
+		await addToLS(id, '/api/meals');
+		setIsCartEmpty(!isCartEmpty);
+	}
+	const categoryList = [
+		{ text: 'All', name: 'all' },
+		{ text: 'Meals', name: 'meals' },
+		{ text: 'Sides', name: 's_ides' },
+		{ text: 'Drinks', name: 'drinks' },
+	];
 
-  return (
-    <section className="meals-main">
-      <section className="meals-section">
-        <section className="searchbar-section">
-          <SearchBar
-            list={filteredItems}
-            setListToShow={(newList) => setListToShow(newList || [])}
-            fullMenu={fullMenu}
-          />
-        </section>
-        <section className="category-button-section">
-          <button
-            onClick={() => handleCategoryClick("all")}
-            className={
-              selectedCategory === "all"
-                ? "category-button selected"
-                : "category-button"
-            }
-          >
-            All
-          </button>
-          <button
-            onClick={() => handleCategoryClick("meals")}
-            className={
-              selectedCategory === "meals"
-                ? "category-button selected"
-                : "category-button"
-            }
-          >
-            Meals
-          </button>
-          <button
-            onClick={() => handleCategoryClick("s_ides")}
-            className={
-              selectedCategory === "s_ides"
-                ? "category-button selected"
-                : "category-button"
-            }
-          >
-            Sides
-          </button>
-          <button
-            onClick={() => handleCategoryClick("drinks")}
-            className={
-              selectedCategory === "drinks"
-                ? "category-button selected"
-                : "category-button"
-            }
-          >
-            Drinks
-          </button>
-        </section>
-        {/* Kolla om listToShow finns, annars sätt en spinner */}
-        {listToShow.map((menuItem: Dish) => (
-          <div key={menuItem._id} className="meals-card">
-            <NavLink
-              to={`/menu/${menuItem._id}`}
-              className="meals-link"
-              onClick={refreshQuantity}
-            >
-              <img
-                src={menuItem.image}
-                alt={`image of ${menuItem.name}`}
-                className="meals-img"
-              />
-              <div className="meals-text">
-                <p>{menuItem.name}</p>
-                <p className="meals-price">{menuItem.price} :-</p>
-              </div>
-            </NavLink>
-            <button
-              className="meals-btn"
-              onClick={() => handleAddToCart(menuItem._id)}
-            >
-              Add to cart <BsCart3 className="btn-icon" />
-            </button>
-          </div>
-        ))}
-      </section>
-      {windowWidth > 1200 ? (
-        <div className="cart-route-container">
-          <CartRoute />
-        </div>
-      ) : null}
-    </section>
-  );
+	return (
+		<section className='meals-main'>
+			<section className='meals-section'>
+				<section className='searchbar-section'>
+					<SearchBar
+						list={filteredItems}
+						setListToShow={(newList) =>
+							setListToShow(newList || [])
+						}
+						fullMenu={fullMenu}
+					/>
+				</section>
+				<section className='category-button-section'>
+					{categoryList.map((category) => (
+						<CategoryButton
+							key={category.name}
+							selectedCategory={selectedCategory}
+							handleCategoryClick={handleCategoryClick}
+							buttonText={category.text}
+							databaseCategoryName={category.name}
+						/>
+					))}
+				</section>
+				{/* Kolla om listToShow finns, annars sätt en spinner */}
+				{listToShow.map((menuItem: Dish) => (
+					<div key={menuItem._id} className='meals-card'>
+						<NavLink
+							to={`/menu/${menuItem._id}`}
+							className='meals-link'
+							onClick={refreshQuantity}
+						>
+							<img
+								src={menuItem.image}
+								alt={`image of ${menuItem.name}`}
+								className='meals-img'
+							/>
+							<div className='meals-text'>
+								<p>{menuItem.name}</p>
+								<p className='meals-price'>
+									{menuItem.price} :-
+								</p>
+							</div>
+						</NavLink>
+						<button
+							className='meals-btn'
+							onClick={() => handleAddToCart(menuItem._id)}
+						>
+							Add to cart <BsCart3 className='btn-icon' />
+						</button>
+					</div>
+				))}
+			</section>
+			{windowWidth > 1200 ? (
+				<div className='cart-route-container'>
+					<CartRoute />
+				</div>
+			) : null}
+		</section>
+	);
 };
 export default Meals;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,111 +1,113 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from 'react';
 // import menu from '../data/menu.json';
-import dishMatch from "../utils/search.ts";
-import { ChangeEvent } from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-import { BsFilter } from "react-icons/bs";
-import FilterMeals from "./FilterMeals.tsx";
-import { SearchBarProps } from "../interfaces/search-and-filter-props.ts";
-import { Dish } from "../interfaces/dish.ts";
-import { useRecoilState } from "recoil";
-import { subState } from "../../src/recoil/subCategoryState.js";
-import "../styles/searchBar.css";
-import axios from "axios";
+import dishMatch from '../utils/search.ts';
+import { ChangeEvent } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BsFilter } from 'react-icons/bs';
+import FilterMeals from './FilterMeals.tsx';
+import { SearchBarProps } from '../interfaces/search-and-filter-props.ts';
+import { Dish } from '../interfaces/dish.ts';
+import { useRecoilState } from 'recoil';
+import { subState } from '../../src/recoil/subCategoryState.js';
+import '../styles/searchBar.css';
+import axios from 'axios';
 
 const SearchBar: React.FC<SearchBarProps> = ({
-  list,
-  setListToShow,
-  fullMenu,
+	list,
+	setListToShow,
+	fullMenu,
 }) => {
-  const [showFilters, setShowFilters] = useRecoilState(subState);
-  const [searchMode, setSearchMode] = useState(false);
-  const [searchInput, setSearchInput] = useState("");
-  const subMenuRef = useRef<HTMLButtonElement | null>(null);
+	const [showFilters, setShowFilters] = useRecoilState(subState);
+	const [searchMode, setSearchMode] = useState(false);
+	const [searchInput, setSearchInput] = useState('');
+	const subMenuRef = useRef<HTMLButtonElement | null>(null);
 
-  // searchMode kontrollerar om man ska söka eller filtrera
-  useEffect(() => {
-    if (searchInput) {
-      setSearchMode(true);
-    } else {
-      setSearchMode(false);
-    }
-  }, [searchInput]);
+	// searchMode kontrollerar om man ska söka eller filtrera
+	useEffect(() => {
+		if (searchInput) {
+			setSearchMode(true);
+		} else {
+			setSearchMode(false);
+		}
+	}, [searchInput]);
 
-  // searchMode sätts till false när man klickar på ett filter i FilterMeals -> då töms input-fältet
-  useEffect(() => {
-    if (!searchMode) {
-      setSearchInput("");
-    }
-  }, [searchMode]);
+	// searchMode sätts till false när man klickar på ett filter i FilterMeals -> då töms input-fältet
+	useEffect(() => {
+		if (!searchMode) {
+			setSearchInput('');
+		}
+	}, [searchMode]);
 
-  //   Söker genom menyn och updaterar listToShow med rätterna som matchar. Om söksträngen är tom sätts listToShow till ursprungslistan
-  // const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
-  // 	const searchString = event.target.value;
-  // 	setSearchInput(searchString);
-  // 	if (searchString) {
-  // 		const searchList: Dish[] | undefined = menu.filter((dish) =>
-  // 			dishMatch(dish.name, searchString)
-  // 		) as Dish[];
-  // 		if (searchList) {
-  // 			setListToShow(searchList);
-  // 		} else {
-  // 			setListToShow([]);
-  // 		}
-  // 	} else {
-  // 		setListToShow(list);
-  // 	}
-  // };
+	//   Söker genom menyn och updaterar listToShow med rätterna som matchar. Om söksträngen är tom sätts listToShow till ursprungslistan
+	// const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+	// 	const searchString = event.target.value;
+	// 	setSearchInput(searchString);
+	// 	if (searchString) {
+	// 		const searchList: Dish[] | undefined = menu.filter((dish) =>
+	// 			dishMatch(dish.name, searchString)
+	// 		) as Dish[];
+	// 		if (searchList) {
+	// 			setListToShow(searchList);
+	// 		} else {
+	// 			setListToShow([]);
+	// 		}
+	// 	} else {
+	// 		setListToShow(list);
+	// 	}
+	// };
 
-  //om uef en funktion som anropas i uef som är asyncron
+	//om uef en funktion som anropas i uef som är asyncron
 
-  const handleSearchChange = async (event: ChangeEvent<HTMLInputElement>) => {
-    const searchString = event.target.value;
-    setSearchInput(searchString);
+	const handleSearchChange = async (event: ChangeEvent<HTMLInputElement>) => {
+		const searchString = event.target.value;
+		setSearchInput(searchString);
 
-    try {
-      if (searchString) {
-        const response = await axios.get(`/api/meals?search=${searchString}`);
-        const searchList: Dish[] = response.data.filter((dish) =>
-          dishMatch(dish.name, searchString)
-        );
-        setListToShow(searchList);
-      } else {
-        setListToShow(list);
-      }
-    } catch (error) {
-      console.log("Error fetching dishes: ", error);
-    }
-  };
+		try {
+			if (searchString) {
+				const response = await axios.get(
+					`/api/meals?search=${searchString}`
+				);
+				const searchList: Dish[] = response.data.filter((dish) =>
+					dishMatch(dish.name, searchString)
+				);
+				setListToShow(searchList);
+			} else {
+				setListToShow(list);
+			}
+		} catch (error) {
+			console.log('Error fetching dishes: ', error);
+		}
+	};
 
-  return (
-    <div className="search-bar">
-      <div className="search-input-container">
-        <AiOutlineSearch />
-        <input
-          type="text"
-          id="search-input"
-          onChange={handleSearchChange}
-          onFocus={() => setShowFilters(false)}
-          value={searchInput}
-        />
-        <button
-          className="filter-btn"
-          onClick={() => setShowFilters(true)}
-          ref={subMenuRef}
-        >
-          <BsFilter />
-        </button>
-      </div>
-      <FilterMeals
-        list={list}
-        setListToShow={setListToShow}
-        searchMode={searchMode}
-        setSearchMode={setSearchMode}
-        subMenuRef={subMenuRef}
-        fullMenu={fullMenu}
-      />
-    </div>
-  );
+	return (
+		<div className='search-bar'>
+			<div className='search-input-container'>
+				<AiOutlineSearch />
+				<input
+					type='text'
+					id='search-input'
+					onChange={handleSearchChange}
+					onFocus={() => setShowFilters(false)}
+					value={searchInput}
+				/>
+				<button
+					className='filter-btn'
+					onClick={() => setShowFilters(true)}
+					ref={subMenuRef}
+				>
+					<BsFilter />
+				</button>
+			</div>
+			<FilterMeals
+				list={list}
+				setListToShow={setListToShow}
+				searchMode={searchMode}
+				setSearchMode={setSearchMode}
+				subMenuRef={subMenuRef}
+				fullMenu={fullMenu}
+			/>
+		</div>
+	);
 };
 
 export default SearchBar;
