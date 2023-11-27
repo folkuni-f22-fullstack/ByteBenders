bcc1b0e577b9d22716d797ee085ffd6462c7812b
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-11-27 11:13:11 +0100

    bugfix

--- src/components/DoneOrderCard.tsx
+++ src/components/DoneOrderCard.tsx
@@ -33,12 +33,14 @@ export default function DoneOrderCard() {
 
   // todo Koppla faktiskt data från cart till Employee gränssnittet
 
-    if (orderData === null) {
-        // Lägg till något laddningsindikator eller annat meddelande medan data hämtas
-        return <section className='loading-container'>
-            <div className="loading-order">Loading...</div>
-        </section>
-    }
+  if (orderData === null) {
+    // Lägg till något laddningsindikator eller annat meddelande medan data hämtas
+    return (
+      <section className="loading-container">
+        <div className="loading-order">Loading...</div>
+      </section>
+    );
+  }
 
   const handleOrderDone = async (order: Order) => {
     try {
@@ -54,49 +56,59 @@ export default function DoneOrderCard() {
 
   const doneOrders = orderData.filter((order) => order.status === "done");
 
-    return (
-        <section className='recieved-order-container'>
-            {doneOrders && doneOrders.map(order => (
-                <div className="recieved-order-card" key={order._id}>
-                    <div className="order-content">
-                        <h1> <div >{order.orderId}</div> </h1>
-                        <div className="extend-order-icons">
-                            {isExpanded === order._id ? (
-                                <button
-                                    onClick={() => setIsExpanded(null)}
-                                    className='close-order-icon'>
-                                    <IoIosArrowUp />
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={() => setIsExpanded(order._id)}
-                                    className='open-order-icon'>
-                                    <IoIosArrowDown />
-                                </button>
-                            )}
-                        </div>
-                    </div >
-                    {isExpanded === order._id && (
-                        <section className='order-info-section'>
-                            <ul className='order-info-list'>
-                                {order.content.map((item) => (
-                                    <li className='order-product-name' key={item.name} >
-                                        {item.name} {item.quantity}x
-                                    </li>
-                                ))}
-                                <hr className="linebreak-current" />
-                            </ul>
-                            <div className='user-comment-section'>
-                                <p className='order-comment'>Comments:</p>
-                                <span className="user-comment">{order.usercomment}</span>
-                            </div>
-                            <div className='send-order-icon'> <RiCheckboxCircleLine />
-                            </div>
-                        </section>
-                    )
-                    }
-                </div >
-            ))}
-        </section>
-    )
+  return (
+    <section className="recieved-order-container">
+      {doneOrders &&
+        doneOrders.map((order) => (
+          <div className="recieved-order-card" key={order._id}>
+            <div className="order-content">
+              <h1>
+                {" "}
+                <div>{order.orderId}</div>{" "}
+              </h1>
+              <div className="extend-order-icons">
+                {isExpanded === order._id ? (
+                  <button
+                    onClick={() => setIsExpanded(null)}
+                    className="close-order-icon"
+                  >
+                    <IoIosArrowUp />
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => setIsExpanded(order._id)}
+                    className="open-order-icon"
+                  >
+                    <IoIosArrowDown />
+                  </button>
+                )}
+              </div>
+            </div>
+            {isExpanded === order._id && (
+              <section className="order-info-section">
+                <ul className="order-info-list">
+                  {order.content.map((item) => (
+                    <li className="order-product-name" key={item.name}>
+                      {item.name} {item.quantity}x
+                    </li>
+                  ))}
+                  <hr className="linebreak-current" />
+                </ul>
+                <div className="user-comment-section">
+                  <p className="order-comment">Comments:</p>
+                  <span className="user-comment">{order.usercomment}</span>
+                </div>
+                <div
+                  className="send-order-icon"
+                  onClick={() => handleOrderDone(order)}
+                >
+                  {" "}
+                  <RiCheckboxCircleLine />
+                </div>
+              </section>
+            )}
+          </div>
+        ))}
+    </section>
+  );
 }
