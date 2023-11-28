3e44def386538065bce586843c5b9f562e855778
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-11-28 11:34:07 +0100

    ikoner anst sida

--- src/routes/DoneOrderRoute.tsx
+++ src/routes/DoneOrderRoute.tsx
@@ -1,15 +1,19 @@
 import DoneOrderCard from "../components/DoneOrderCard";
-import '../styles/OrderCards.css'
+import "../styles/OrderCards.css";
+import { MdDone } from "react-icons/md";
 
 export default function DoneOrderRoute() {
-    return (
-        <>
-            <div className="order-route-header">
-                <div className="order-page-employee">
-                    <h1> Done Orders </h1>
-                </div>
-            </div>
-            <DoneOrderCard />
-        </>
-    );
+  return (
+    <>
+      <div className="order-route-header">
+        <div className="order-page-employee">
+          <h1 className="employee-h1">
+            {" "}
+            Done Orders <MdDone className="employee-icon-done" />{" "}
+          </h1>
+        </div>
+      </div>
+      <DoneOrderCard />
+    </>
+  );
 }
--- src/routes/RecOrderRoute.tsx
+++ src/routes/RecOrderRoute.tsx
@@ -1,15 +1,19 @@
 import RecievedOrderCard from "../components/RecievedOrderCard";
-import '../styles/OrderCards.css'
+import "../styles/OrderCards.css";
+import { BiSolidPencil } from "react-icons/bi";
 
 export default function RecOrderRoute() {
-    return (
-        <>
-            <div className="order-route-header">
-                <div className="order-page-employee">
-                    <h1> Received Orders </h1>
-                </div>
-            </div>
-            <RecievedOrderCard />
-        </>
-    );
+  return (
+    <>
+      <div className="order-route-header">
+        <div className="order-page-employee">
+          <h1 className="employee-h1">
+            {" "}
+            Received Orders <BiSolidPencil className="employee-icon" />{" "}
+          </h1>
+        </div>
+      </div>
+      <RecievedOrderCard />
+    </>
+  );
 }
