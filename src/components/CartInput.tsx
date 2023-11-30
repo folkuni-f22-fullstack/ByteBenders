6525683f99806b46429eb35e9f6dd834e4f2edd7
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-11-30 19:49:35 +0100

    fixed some ts errors and cleaned up a bit

--- src/components/CartInput.tsx
+++ src/components/CartInput.tsx
@@ -1,187 +1,184 @@
-import { useEffect, useState, useRef } from "react";
-import { MdLabelOutline } from "react-icons/md";
-import { useRecoilState } from "recoil";
-import { DishInCart, Dish } from "../interfaces/dish.ts";
-import { getCartQuantity } from "../utils/general.ts";
-import SendCartData from "./CartSendDb.tsx";
-import { orderState } from "../recoil/orderState.js";
-import { cartState } from "../recoil/cartNumberState.js";
-import axios from "axios";
-import { signal } from "@preact/signals-react";
+import React, { useEffect, useState, useRef } from 'react';
+import { MdLabelOutline } from 'react-icons/md';
+import { useRecoilState } from 'recoil';
+import { DishInCart, Dish } from '../interfaces/dish.ts';
+import { getCartQuantity } from '../utils/general.ts';
+import SendCartData from './CartSendDb.tsx';
+import { orderState } from '../recoil/orderState.js';
+import { cartState } from '../recoil/cartNumberState.js';
+import { signal } from '@preact/signals-react';
+import { CartInputProps } from '../interfaces/cart.ts';
 
 export let promo = signal(0);
 export let totalPrice = signal(0);
-function CartInput() {
-  const [currentOrder, setCurrentOrder] = useRecoilState(orderState);
-  const [cartCopy, setCartCopy] = useState<DishInCart[]>([]);
-  const [cartItem, setCartItem] = useState<Dish[]>([]);
-  let [isPromo, setIsPromo] = useState("");
-  const [cartItems, setCartItems] = useRecoilState(cartState);
-  const [isNameValid, setIsNameValid] = useState(true);
-  const [isMailValid, setIsMailValid] = useState(true);
-  const [isComment, setIsComment] = useState(true);
-  const [isPromoCorrect, setIsPromoCorrect] = useState(true);
-  const customerNameRef = useRef(null);
-  const customerMailRef = useRef(null);
-  const customerCommentRef = useRef(null);
+function CartInput({ cartCopy }: CartInputProps): JSX.Element {
+	const [currentOrder, setCurrentOrder] = useRecoilState(orderState);
+	let [isPromo, setIsPromo] = useState<string>('');
+	const [cartItems, setCartItems] = useRecoilState(cartState);
+	const [isNameValid, setIsNameValid] = useState<boolean>(true);
+	const [isMailValid, setIsMailValid] = useState<boolean>(true);
+	const [isComment, setIsComment] = useState<boolean>(true);
+	const [isPromoCorrect, setIsPromoCorrect] = useState<boolean>(true);
+	const customerNameRef = useRef<HTMLInputElement>(null);
+	const customerMailRef = useRef<HTMLInputElement>(null);
+	const customerCommentRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    axios
-      .get("/api/meals")
-      .then((response) => setCartItem(response.data))
-      .catch((error) => console.error("error feching meals", error));
-  }, []);
+	// Count total price
+	function calculateTotalPrice() {
+		let total = 0;
+		cartCopy.forEach((item) => {
+			total += item.total;
+		});
+		return total;
+	}
 
-  useEffect(() => {
-    const updatedCart = JSON.parse(localStorage.getItem("cart")) || [];
-    setCartCopy(updatedCart);
-    // isCartEmpty toggles from Meals.jsx
-  }, [localStorage.getItem("cart")]);
+	// Update promo code
+	function handlePromoCodeChange(e) {
+		const newPromoCode = e.target.value;
+		setIsPromoCorrect(true);
+		setIsPromo(newPromoCode);
+		localStorage.setItem('promo-code', newPromoCode);
+	}
 
-  // Count total price
-  function calculateTotalPrice() {
-    let total = 0;
-    cartCopy.forEach((item) => {
-      total += item.total;
-    });
-    return total;
-  }
+	// Handle dicount
+	function promoCode() {
+		const storedPromoCode = localStorage.getItem('promo-code') || '';
+		setIsPromo(storedPromoCode);
 
-  // Update promo code
-  function handlePromoCodeChange(e) {
-    const newPromoCode = e.target.value;
-    setIsPromoCorrect(true);
-    setIsPromo(newPromoCode);
-    localStorage.setItem("promo-code", newPromoCode);
-  }
+		const storedDiscount = localStorage.getItem('new-price');
+		promo.value = JSON.parse(storedDiscount || '0');
 
-  // Handle dicount
-  function promoCode() {
-    const storedPromoCode = localStorage.getItem("promo-code") || "";
-    setIsPromo(storedPromoCode);
+		if (storedPromoCode === 'discount20%') {
+			const discount = Math.round(totalPrice.value * 0.8);
+			promo.value = discount;
+			localStorage.setItem('new-price', JSON.stringify(promo.value));
+		} else if (isPromo === '') {
+			promo.value = 0;
+			setIsPromo('');
+		} else {
+			setIsPromoCorrect(false);
+			promo.value = 0;
+			setIsPromo('Promo code not correct');
+		}
+	}
 
-    const storedDiscount = localStorage.getItem("new-price");
-    promo.value = JSON.parse(storedDiscount);
+	useEffect(() => {
+		promoCode();
+	}, []);
 
-    if (storedPromoCode === "discount20%") {
-      const discount = Math.round(totalPrice * 0.8);
-      promo.value = discount;
-      localStorage.setItem("new-price", JSON.stringify(promo.value));
-    } else if (isPromo === "") {
-      promo.value = 0;
-      setIsPromo("");
-    } else {
-      setIsPromoCorrect(false);
-      promo.value = 0;
-      setIsPromo("Promo code not correct");
-    }
-  }
+	// Update discount and total price
+	useEffect(() => {
+		totalPrice.value = calculateTotalPrice();
+		promoCode();
+	}, [cartCopy]);
 
-  useEffect(() => {
-    promoCode();
-  }, []);
+	useEffect(() => {
+		setCartItems(getCartQuantity());
+	}, [cartCopy]);
 
-  // Update discount and total price
-  useEffect(() => {
-    totalPrice.value = calculateTotalPrice();
-    promoCode();
-  }, [cartCopy]);
+	return (
+		<>
+			{/* Total price */}
+			<div className={`cart-total-container`}>
+				<input
+					ref={customerCommentRef}
+					className='user-input'
+					id='customer-mail'
+					type='text'
+					placeholder='Customize your order'
+					onChange={() => setIsComment(true)}
+				/>
+			</div>
+			{/* "First and last name" input field */}
+			<div
+				className={`userinput-container ${
+					!isNameValid ? 'invalid-input' : ''
+				}`}
+			>
+				<input
+					ref={customerNameRef}
+					className='user-input'
+					id='customer-name'
+					type='text'
+					placeholder='First and last name'
+					onChange={() => setIsNameValid(true)}
+				/>
+			</div>
 
-  useEffect(() => {
-    setCartItems(getCartQuantity());
-  }, [cartCopy]);
-
-  return (
-    <>
-      {/* Total price */}
-      <div className={`cart-total-container`}>
-        <input
-          ref={customerCommentRef}
-          className="user-input"
-          id="customer-mail"
-          type="text"
-          placeholder="Customize your order"
-          onChange={() => setIsComment(true)}
-        />
-      </div>
-      {/* "First and last name" input field */}
-      <div
-        className={`userinput-container ${!isNameValid ? "invalid-input" : ""}`}
-      >
-        <input
-          ref={customerNameRef}
-          className="user-input"
-          id="customer-name"
-          type="text"
-          placeholder="First and last name"
-          onChange={() => setIsNameValid(true)}
-        />
-      </div>
-
-      {/* "Mail address" input field */}
-      <div
-        className={`userinput-container ${!isMailValid ? "invalid-input" : ""}`}
-      >
-        <input
-          ref={customerMailRef}
-          className="user-input"
-          id="customer-mail"
-          type="text"
-          placeholder="Mail address"
-          onChange={() => setIsMailValid(true)}
-        />
-      </div>
-      {/* Promo */}
-      <div className="cart-promo-container">
-        <div className="promo-icon-input">
-          <MdLabelOutline size={20} className="promo-icon" />
-          <input
-            className={`promo-code ${!isPromoCorrect ? "invalid-promo" : ""}`}
-            type="text"
-            placeholder="Promo code"
-            value={
-              !currentOrder.isOrdered && !currentOrder.isWaiting ? isPromo : ""
-            }
-            onChange={handlePromoCodeChange}
-            onClick={() => setIsPromo("")}
-          />
-        </div>
-        <button className="apply-promo-button" onClick={promoCode}>
-          Apply
-        </button>
-      </div>
-      <div className="total-price-div">
-        <p className="total-text">Total:</p>
-        <div className="price">
-          {!currentOrder.isOrdered && !currentOrder.isWaiting ? (
-            <>
-              {promo.value !== 0 && <p className="new-price">{promo} :-</p>}
-              <p
-                className={
-                  promo.value === 0 ? "total-price" : "total-price--crossed"
-                }
-              >
-                {totalPrice} :-
-              </p>
-            </>
-          ) : (
-            <>
-              <p className="total-price">0 :-</p>
-            </>
-          )}
-        </div>
-      </div>
-      {/* SendCartData component */}
-      <SendCartData
-        customerNameRef={customerNameRef}
-        customerMailRef={customerMailRef}
-        customerCommentRef={customerCommentRef}
-        setIsNameValid={setIsNameValid}
-        setIsMailValid={setIsMailValid}
-        setIsComment={setIsComment}
-      />
-    </>
-  );
+			{/* "Mail address" input field */}
+			<div
+				className={`userinput-container ${
+					!isMailValid ? 'invalid-input' : ''
+				}`}
+			>
+				<input
+					ref={customerMailRef}
+					className='user-input'
+					id='customer-mail'
+					type='text'
+					placeholder='Mail address'
+					onChange={() => setIsMailValid(true)}
+				/>
+			</div>
+			{/* Promo */}
+			<div className='cart-promo-container'>
+				<div className='promo-icon-input'>
+					<MdLabelOutline size={20} className='promo-icon' />
+					<input
+						className={`promo-code ${
+							!isPromoCorrect ? 'invalid-promo' : ''
+						}`}
+						type='text'
+						placeholder='Promo code'
+						value={
+							!currentOrder.isOrdered && !currentOrder.isWaiting
+								? isPromo
+								: ''
+						}
+						onChange={handlePromoCodeChange}
+						onClick={() => setIsPromo('')}
+					/>
+				</div>
+				<button className='apply-promo-button' onClick={promoCode}>
+					Apply
+				</button>
+			</div>
+			<div className='total-price-div'>
+				<p className='total-text'>Total:</p>
+				<div className='price'>
+					{!currentOrder.isOrdered && !currentOrder.isWaiting ? (
+						<>
+							{promo.value !== 0 && (
+								<p className='new-price'>{promo} :-</p>
+							)}
+							<p
+								className={
+									promo.value === 0
+										? 'total-price'
+										: 'total-price--crossed'
+								}
+							>
+								{totalPrice} :-
+							</p>
+						</>
+					) : (
+						<>
+							<p className='total-price'>0 :-</p>
+						</>
+					)}
+				</div>
+			</div>
+			{/* SendCartData component */}
+			<SendCartData
+				customerNameRef={customerNameRef}
+				customerMailRef={customerMailRef}
+				customerCommentRef={customerCommentRef}
+				setIsNameValid={setIsNameValid}
+				setIsMailValid={setIsMailValid}
+				setIsComment={setIsComment}
+			/>
+		</>
+	);
 }
 
 export default CartInput;
