452b44745f9d2c42dc9e22e2ae62adaa864f6e96
Hollgy <karlgustav.ek@hotmail.com>
2023-12-01 14:20:24 +0100

    animations added

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { Dish } from "../interfaces/dish";
 import { refreshQuantity } from "../utils/quantityChange";
 import { BsCart3 } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 type MealCardProps = {
   menuItem: Dish;
@@ -11,7 +12,11 @@ type MealCardProps = {
 
 const MealCard: React.FC<MealCardProps> = ({ menuItem, handleAddToCart }) => {
   return (
-    <div key={menuItem._id} className="meals-card">
+    <motion.div
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5, delay: 0 }}
+    key={menuItem._id} className="meals-card">
       <NavLink
         to={`/menu/${menuItem._id}`}
         className="meals-link"
@@ -34,7 +39,7 @@ const MealCard: React.FC<MealCardProps> = ({ menuItem, handleAddToCart }) => {
         <p className="add-cart">Add to cart </p>{" "}
         <BsCart3 className="btn-icon" />
       </button>
-    </div>
+    </motion.div>
   );
 };
 
